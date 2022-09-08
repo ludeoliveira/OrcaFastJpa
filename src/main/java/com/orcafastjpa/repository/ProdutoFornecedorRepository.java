d3ba28c8d4d5531052b2a5cf7332fa7ad200e0ac
Gr-Emile <emile.ramos@live.com>
2022-09-08 11:13:12 -0300

    cosultar preco por id produto  - entidade produtofornecedor -Incompleto

--- src/main/java/com/orcafastjpa/repository/ProdutoFornecedorRepository.java
+++ src/main/java/com/orcafastjpa/repository/ProdutoFornecedorRepository.java
@@ -1,10 +1,15 @@
 package com.orcafastjpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.orcafastjpa.entidades.ProdutoFornecedor;
 
 @Repository
 public interface ProdutoFornecedorRepository extends JpaRepository<ProdutoFornecedor, Long>{
+	@Query("select pf from ProdutoFornecedor pf, Produto prod where pf.produto = prod.id and  prod.id = ?1")
+	List <ProdutoFornecedor> findByProdutoFornecedor(Long idproduto);
 }
